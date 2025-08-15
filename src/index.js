29066d9eb561543e5eacb0e76ceb1a35a7fdab1c
Asqarbek Olimov <88034288+asqarbekolimov@users.noreply.github.com>
2025-08-15 05:30:47 +0000

    feat: implement toReadable function for number conversion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,59 @@
-module.exports = function toReadable(/* number */) {
-  throw new Error('Not implemented');
+module.exports = function toReadable(num) {
+  const ones = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
+
+  const tens = [
+    '',
+    '',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+  ];
+
+  if (num < 20) {
+    return ones[num];
+  }
+
+  if (num < 100) {
+    const t = Math.floor(num / 10);
+    const o = num % 10;
+    return o ? `${tens[t]} ${ones[o]}` : tens[t];
+  }
+
+  if (num < 1000) {
+    const h = Math.floor(num / 100);
+    const remainder = num % 100;
+    let result = `${ones[h]} hundred`;
+    if (remainder) {
+      result += ` ${toReadable(remainder)}`;
+    }
+    return result;
+  }
+
+  return 'number out of range';
 };
